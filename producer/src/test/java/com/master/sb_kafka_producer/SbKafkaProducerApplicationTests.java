66714c8dd570cb930439769c8a941ab477a63591
Ulisses Alves <ulisalves@gmail.com>
2026-08-28 10:59:14 -0300

    first commit

---
+++ producer/src/test/java/com/master/sb_kafka_producer/SbKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.master.sb_kafka_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbKafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
